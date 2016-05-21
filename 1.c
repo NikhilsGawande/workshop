34ddb153857a1414306fa46c39155b76bbba8e20
Nikhil Gawande <nikhilgawande21@gmail.com>
2016-05-21 17:44:06 +0000

    nik modified 1.c

--- 1.c
+++ 1.c
@@ -1,8 +1,7 @@
 #include <unistd.h>
 #include <stdio.h>
 int main() {
-	//printf("bye\n");
-	fork();     //makes copy of running program
+	printf("bye\n");    //makes copy of running program
 	printf("hi\n");
 	return 0;
 }
